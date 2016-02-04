cbd6d9350eb1b1433bbb08c8d74b76d8b6829b8e
Michael Luo <michaell@zillow.com>
2016-02-03 19:48:47 -0800

    Adding in jobrequest logic and model. also fixed a password hasing error

--- hashPasswords/passwords.py
+++ hashPasswords/passwords.py
@@ -1,4 +1,4 @@
 from hashlib import sha512
 
 def hashPassword( password, username ):
-    return sha512('assystedisomgsogreat'+password+'username').hexdigest()
+    return sha512('assystedisomgsogreat'+password+username).hexdigest()
--- jobRequest/models.py
+++ jobRequest/models.py
@@ -1,6 +1,14 @@
-from mongoengine import connect
+from mongoengine import connect, document
+from mongoengine.fields import *
 
 connect('assysted_DB')
 
-
-
+class JobRequest(document.Document):
+    userId = ObjectIdField(required=True) #user objectID
+    email = StringField(required=True)
+    stillSearching = BooleanField(required=True)
+    timeOfRequest = DateTimeField()
+    description = StringField(required=True)
+    userSubmitPrice = IntField(required=True)
+    listOfBidders = ListField( field=[ObjectIdField, StringField ], required=True )
+    listOfBids = ListField( field=IntField, required=True )

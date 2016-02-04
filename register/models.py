5f622df47b6bc24187c3fbdd1dc48e78b12cf314
Michael Luo <michaell@zillow.com>
2016-02-03 20:00:58 -0800

    Also forgot to import document

--- register/models.py
+++ register/models.py
@@ -1,9 +1,9 @@
-from mongoengine import connect
+from mongoengine import connect, document
 from mongoengine.fields import *
 
 connect('assysted_DB')
 
-class User(Document):
+class User(document.Document):
     email = StringField(required=True)
     password = StringField(required=True)
     activated = BooleanField(required=True)

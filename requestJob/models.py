91c1f4299754f3764ff286aba109c3f9b88fd20c
Michael Luo <michaell@zillow.com>
2016-02-03 21:05:58 -0800

    Testing

--- requestJob/models.py
+++ requestJob/models.py
@@ -10,6 +10,6 @@ class JobRequest(document.Document):
     timeOfRequest = DateTimeField()
     description = StringField(required=True)
     userSubmitPrice = IntField(required=True)
-    listOfBidders = ListField( field=ListField, required=True )
+    listOfBidders = ListField( field=StringField, required=True )
     listOfBids = ListField( field=IntField, required=True )
 

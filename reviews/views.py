60fe8393a4834525afc085f463bcdf00b77b7484
Michael Luo <michaell@zillow.com>
2016-02-29 22:33:50 -0800

    Added updating of user being reviewed

--- reviews/views.py
+++ reviews/views.py
@@ -4,6 +4,7 @@ from login.loginHelper import check_user_id_exists
 from reviews.models import Rating
 from requestJob.helper_func import create_return_dict
 from json import dumps
+from register.models import User
 
 # Create your views here.
 
@@ -17,10 +18,10 @@ def index(request):
     ratingPost = request.POST.get('rating', '')
     descriptionPost = request.POST.get('description', '')
 
-    return HttpResponse(dumps(putReviewIn(reviewerPost, userBeingReviewedPost, ratingPost, descriptionPost)))
+    return HttpResponse(dumps(put_review_in(reviewerPost, userBeingReviewedPost, ratingPost, descriptionPost)))
 
 
-def putReviewIn(reviewerPost, userBeingReviewedPost, ratingPost, descriptionPost):
+def put_review_in(reviewerPost, userBeingReviewedPost, ratingPost, descriptionPost):
     '''
     Puts review into database if User IDs for reviewer and person being reviewed exist
     :param reviewerPost:
@@ -30,9 +31,22 @@ def putReviewIn(reviewerPost, userBeingReviewedPost, ratingPost, descriptionPost
     :return:
     '''
     if check_user_id_exists(reviewerPost) and check_user_id_exists(userBeingReviewedPost):
+        update_user_rating(userBeingReviewedPost)
         ratingToAdd = Rating(reviewer=reviewerPost, userBeingReviewed=userBeingReviewedPost, rating=int(ratingPost), description=descriptionPost)
         ratingToAdd.save()
         return create_return_dict(1, 'Successfully added review!')
 
     return create_return_dict(-1, 'Users do not exist')
 
+def update_user_rating(userBeingReviewedInput):
+    '''
+    Updates the user being reviewed's average rating
+    :param userBeingReviewed: user to update
+    :return: nothing
+    '''
+    setOfUserRatings = Rating.objects.filter(userBeingReviewed=userBeingReviewedInput)
+    newAvg = setOfUserRatings.aggregate_average("rating")
+    userToEdit = User.objects.get(id=userBeingReviewedInput)
+    userToEdit.avgRating = newAvg
+    userToEdit.numRatings = setOfUserRatings.count()
+    userToEdit.save()
